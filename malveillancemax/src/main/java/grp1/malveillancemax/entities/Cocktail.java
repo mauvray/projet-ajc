516cb0f56139df62d2bf586ce4a7593e0575c7d1
Blackychan25 <sorokevin25@hotmail.fr>
2024-05-02 15:53:45 +0200

    Update Cocktail

--- malveillancemax/src/main/java/grp1/malveillancemax/entities/Cocktail.java
+++ malveillancemax/src/main/java/grp1/malveillancemax/entities/Cocktail.java
@@ -1,6 +1,37 @@
 package grp1.malveillancemax.entities;
 
+import java.util.Set;
+
+import jakarta.persistence.Transient;
+
 public class Cocktail extends Boisson{
 
+    // @Transient
+    private Set<AlcoolFort> alcools;
     
-}
+    // @Transient
+    private Set<Soft> softs;
+    
+    public Cocktail(){
+        
+    }
+
+    public Cocktail(String name, double prix, Set<AlcoolFort> alcools) {
+        super(name, prix);
+        this.alcools = alcools;
+    }
+
+    
+    // public Cocktail(String name, double prix, Set<Soft> softs) {
+    //     super(name, prix);
+    //     this.softs = softs;
+    // }
+
+    public Cocktail(String name, double prix, Set<AlcoolFort> alcools, Set<Soft> softs) {
+        super(name, prix);
+        this.alcools = alcools;
+        this.softs = softs;
+    }
+    
+    
+ }
