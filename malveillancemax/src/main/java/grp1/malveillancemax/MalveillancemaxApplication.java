21283260dd6fa2170d6537f395fdc745e13ec593
Afrisson <alex.frisson@gmail.com>
2024-05-02 14:51:39 +0200

    Initialize spring

---
+++ malveillancemax/src/main/java/grp1/malveillancemax/MalveillancemaxApplication.java
@@ -0,0 +1,13 @@
+package grp1.malveillancemax;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MalveillancemaxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MalveillancemaxApplication.class, args);
+	}
+
+}
---
+++ malveillancemax/src/test/java/grp1/malveillancemax/MalveillancemaxApplicationTests.java
@@ -0,0 +1,13 @@
+package grp1.malveillancemax;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MalveillancemaxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
