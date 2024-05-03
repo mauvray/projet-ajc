110c6197415d105f228ed3949e809598e9f19366
Quentin F <skillzz520@gmail.com>
2024-05-03 11:37:40 +0200

    implement test

--- malveillancemax/src/test/java/grp1/malveillancemax/AppTest.java
+++ malveillancemax/src/test/java/grp1/malveillancemax/AppTest.java
@@ -2,8 +2,10 @@ package grp1.malveillancemax;
 
 import grp1.malveillancemax.entities.Biere;
 import grp1.malveillancemax.entities.Cidre;
+import grp1.malveillancemax.entities.Planche;
 import grp1.malveillancemax.entities.ServiceBiere;
 import grp1.malveillancemax.entities.ServiceCidre;
+import grp1.malveillancemax.entities.Tapas;
 
 public class AppTest{
     public static void main(String[] args) {
@@ -12,5 +14,11 @@ public class AppTest{
 
         Biere biere1 = new Biere("Chouffe",3,null,ServiceBiere.Pression);
         System.out.println(biere1.getPrix());
+
+        Planche planche1 = new Planche("charcuterie",12.25);
+        System.out.println(planche1.getNom());
+
+        Tapas tapas1 = new Tapas("flamme munster", 4.5);
+        System.out.println(tapas1.getNom());
     }
 }
---
+++ malveillancemax/src/test/java/grp1/malveillancemax/services/PlancheSerivceTest.java
@@ -0,0 +1,47 @@
+package grp1.malveillancemax.services;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import jakarta.transaction.Transactional;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+
+import grp1.malveillancemax.exceptions.PlancheException;
+import grp1.malveillancemax.entities.Planche;
+
+@SpringBootTest
+@Transactional
+@Rollback
+class PlancheServiceTest {
+
+	@Autowired
+	private PlancheService plancheSrv;
+
+	@Test
+	void getByIdExceptionTest() {
+		assertThrows(PlancheException.class, () -> {
+			plancheSrv.getById(9999999L);
+		});
+	}
+
+	@Test
+	// @Commit
+	void createPlancheTest() {
+		assertThrows(PlancheException.class, () -> {
+			plancheSrv.create(new Planche("", 10.));
+		});
+		assertThrows(PlancheException.class, () -> {
+			plancheSrv.create(new Planche(null, 10.));
+		});
+        assertThrows(PlancheException.class, () -> {
+			plancheSrv.create(new Planche("test1", 0.));
+		});
+		assertNotNull(plancheSrv.getById(plancheSrv.create(new Planche("test1", 10.)).getId()));
+	}
+
+}
