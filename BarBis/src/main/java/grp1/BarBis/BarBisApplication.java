edabb9f78cafe954df812abcd125024458d3b4df
InesPasDinspi <161725851+InesPasDinspi@users.noreply.github.com>
2024-05-06 09:08:14 +0200

    2e projet bar

---
+++ BarBis/src/main/java/grp1/BarBis/BarBisApplication.java
@@ -0,0 +1,13 @@
+package grp1.BarBis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarBisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BarBisApplication.class, args);
+	}
+
+}
